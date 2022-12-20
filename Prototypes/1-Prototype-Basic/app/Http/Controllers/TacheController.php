fb253f397197e26e54336a1126852bcd36d3aed4
stitou-nada <nadastito1087@gmail.com>
2022-12-20 13:09:17 +0100

    auth

--- Prototypes/1-Prototype-Basic/app/Http/Controllers/TacheController.php
+++ Prototypes/1-Prototype-Basic/app/Http/Controllers/TacheController.php
@@ -7,33 +7,19 @@ use Illuminate\Http\Request;
 
 class TacheController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $tache = tache::all();
         return view('tache.index',compact('tache'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('tache.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         tache::create([
@@ -42,46 +28,25 @@ class TacheController extends Controller
         return redirect('tache');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\tache  $tache
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(tache $tache)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\tache  $tache
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(tache $tache)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\tache  $tache
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, tache $tache)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\tache  $tache
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy( $id)
     {
        tache::find($id)
