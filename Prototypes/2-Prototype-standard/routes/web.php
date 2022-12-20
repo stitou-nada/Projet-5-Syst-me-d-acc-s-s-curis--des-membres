91bb0410936b5444c9c5d2c8ef3f7bdc2e5a98ec
stitou-nada <nadastito1087@gmail.com>
2022-12-20 16:58:30 +0100

    prototype standard

---
+++ Prototypes/2-Prototype-standard/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\TacheController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/tache",[TacheController::class,'index']);
+Route::get("/create",[TacheController::class,'create'])->name('tache.create');
+Route::post("/store",[TacheController::class,'store'])->name('tache.store');;
+Route::delete("/delete/{id}",[TacheController::class,'destroy'])->name('tache.delete');;
