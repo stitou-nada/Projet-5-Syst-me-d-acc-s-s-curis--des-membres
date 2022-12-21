2967d51edbbd6229ca52b67522c8eea91688738b
stitou-nada <nadastito1087@gmail.com>
2022-12-21 13:07:46 +0100

    google

--- Prototypes/2-Prototype-standard/app/Http/Controllers/googleController.php
+++ Prototypes/2-Prototype-standard/app/Http/Controllers/googleController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use PhpParser\Node\Stmt\TryCatch;
 
 class googleController extends Controller
 {
-    function
+  function redirect(){
+
+  return Socialite::driver('google')->redirect();
+  }
+
+   function callbackGoogle(){
+       try {
+           $google_user= Socialite::driver('google')->user();
+          $user = User::where('google_id',$google_user->getId())->first();
+          if (! $user) {
+            $add_user=User::Create([
+                 "name" =>$google_user->getName(),
+                 "email"=>$google_user->getEmail(),
+                 "google_id"=>$google_user->getId(),
+            ]);
+            Auth::login($add_user);
+            return redirect()->intended('dashboard');
+
+          }
+          else{
+            Auth::login($user);
+            return redirect()->intended('dashboard');
+          }
+       } catch (\Throwable $th) {
+        //throw $th;
+        dd('eruur'.$th->getMessage());
+       }
+   }
 }
--- Prototypes/2-Prototype-standard/routes/web.php
+++ Prototypes/2-Prototype-standard/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\googleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 require __DIR__.'/auth.php';
+
+Route::get('auth/google',[googleController::class,'redirect'])->name('google-auth');
+Route::get('auth/google/call-back',[googleController::class,'callbackGoogle'])->name('callbackGoogle');
