4809fb6a2b78918c3aa7fcd27be8a93763668b57
stitou-nada <nadastito1087@gmail.com>
2022-12-22 10:36:53 +0100

    add

--- Prototypes/liveCoding/resources/views/tache/create.blade.php
+++
@@ -1,8 +0,0 @@
-
-
-Ajouter tache:
-<form action="{{route('tache.store')}}" method="POST">
-    @csrf
-    <input type="text" placeholder="nom" name="name"></br>
-    <button class="btn btn-primary">Ajouter</button>
-</form>
